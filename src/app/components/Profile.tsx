f527e4e3837b0bca6438ac258d5ae2cd80e6e84b
klleriston <klleandrade@outlook.com>
2024-11-24 01:13:03 -0300

    feat: add desc

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -6,10 +6,13 @@ export function Profile() {
     return (
         <div className="flex flex-col items-center">
             <h1 className="text-2xl font-bold text-gray-900 dark:text-white">
-                <TypeAnimation sequence={["Klleriston Andrade", 2000]} repeat={Infinity} />
+                <TypeAnimation sequence={["Klleriston Andrade", 3000]} repeat={Infinity} cursor={false} />
             </h1>
+            <h3 className="mt-2 text-gray-700 dark:text-gray-300 text-sm">
+                <TypeAnimation sequence={["Desenvolvedor", 3000]} cursor={false} />
+            </h3>
             <h3 className="mt-2 text-gray-700 dark:text-gray-300">
-                <TypeAnimation sequence={["Node.js | TypeScript | Java | GCP | React", 2000]} repeat={Infinity} />
+                <TypeAnimation sequence={["Node.js | TypeScript | Java | GCP | React", 2000]} cursor={false} />
             </h3>
         </div>
     );
